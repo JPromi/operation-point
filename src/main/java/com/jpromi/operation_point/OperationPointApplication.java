3170b69d82fd6279da64241db644a88ff7845d8d
JPromi <jonasmpr05@gmail.com>
2025-06-03 06:59:56 +0200

    init

---
+++ src/main/java/com/jpromi/operation_point/OperationPointApplication.java
@@ -0,0 +1,13 @@
+package com.jpromi.operation_point;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpromi/operation_point/OperationPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpromi.operation_point;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
