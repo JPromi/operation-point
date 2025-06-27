aa2f1f1f3f48d7e273a555d6aaa2940a3f7e45f9
JPromi <jonasmpr05@gmail.com>
2025-06-27 22:04:12 +0200

    add edit firedepartment (switch to unit, css) not done yet

--- src/main/java/com/jpromi/operation_point/controller/AdminController.java
+++ src/main/java/com/jpromi/operation_point/controller/AdminController.java
@@ -1,15 +1,20 @@
 package com.jpromi.operation_point.controller;
 
 import com.jpromi.operation_point.enitiy.Firedepartment;
+import com.jpromi.operation_point.model.FiredepartmentForm;
 import com.jpromi.operation_point.repository.FiredepartmentRepository;
 import com.jpromi.operation_point.service.FiredepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin")
@@ -39,4 +44,38 @@ public class AdminController {
         model.addAttribute("firedepartments", firedepartments);
         return "admin/firedepartment-list";
     }
+
+    @GetMapping("/dashboard/firedepartment/{uuid}")
+    public String firedepartmentDetail(@PathVariable UUID uuid, Model model) {
+        Optional<Firedepartment> firedepartment = firedepartmentRepository.findByUuid(uuid);
+        if (firedepartment.isEmpty()) {
+            return "redirect:/admin/dashboard/firedepartment";
+        }
+        model.addAttribute("firedepartment", firedepartment.get());
+        return "admin/firedepartment-details";
+    }
+
+    @PostMapping("/dashboard/firedepartment/{uuid}")
+    public String updateFiredepartment(@PathVariable UUID uuid, FiredepartmentForm updatedFiredepartment) {
+        if(updatedFiredepartment.getIsWrongAssignment() != null && updatedFiredepartment.getIsWrongAssignment()) {
+            return "redirect:/admin/dashboard/firedepartment/" + uuid + "?error=wrongAssignment";
+        } else {
+            Optional<Firedepartment> existingFiredepartment = firedepartmentRepository.findByUuid(uuid);
+            if (existingFiredepartment.isPresent()) {
+                Firedepartment firedepartment = existingFiredepartment.get();
+                firedepartment.setFriendlyName(updatedFiredepartment.getFriendlyName());
+                firedepartment.setAtFireDepartmentId(updatedFiredepartment.getAtFireDepartmentId());
+                firedepartment.setAddressCity(updatedFiredepartment.getAddressCity());
+                firedepartment.setAddressStreet(updatedFiredepartment.getAddressStreet());
+                firedepartment.setAddressZipcode(updatedFiredepartment.getAddressZipcode());
+                firedepartment.setAddressCountry(updatedFiredepartment.getAddressCountry());
+                firedepartment.setIsVolunteer(updatedFiredepartment.getIsVolunteer());
+                firedepartment.setWebsite(updatedFiredepartment.getWebsite());
+
+                firedepartmentRepository.save(firedepartment);
+            }
+            return "redirect:/admin/dashboard/firedepartment/" + uuid;
+        }
+    }
+
 }
---
+++ src/main/java/com/jpromi/operation_point/model/FiredepartmentForm.java
@@ -0,0 +1,16 @@
+package com.jpromi.operation_point.model;
+
+import lombok.Data;
+
+@Data
+public class FiredepartmentForm {
+    private String friendlyName;
+    private String atFireDepartmentId;
+    private String addressCity;
+    private String addressStreet;
+    private String addressZipcode;
+    private String addressCountry;
+    private String website;
+    private Boolean isVolunteer = false;
+    private Boolean isWrongAssignment = false;
+}
