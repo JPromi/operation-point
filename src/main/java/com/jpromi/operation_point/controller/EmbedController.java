0c8a9f9820a9c1580f3234e57c078cfc96ef4787
JPromi <jonasmpr05@gmail.com>
2025-09-22 11:24:08 +0200

    add la, bl view as vector embed

--- src/main/java/com/jpromi/operation_point/controller/EmbedController.java
+++ src/main/java/com/jpromi/operation_point/controller/EmbedController.java
@@ -5,15 +5,20 @@ import com.jpromi.operation_point.enitiy.Operation;
 import com.jpromi.operation_point.mapper.LocationStatisticResponseMapper;
 import com.jpromi.operation_point.model.LocationStatisticResponse;
 import com.jpromi.operation_point.repository.CrawlServiceRepository;
+import com.jpromi.operation_point.repository.OperationRepository;
 import com.jpromi.operation_point.service.OperationService;
+import com.jpromi.operation_point.service.OperationVariableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,14 +36,17 @@ public class EmbedController {
     private CrawlServiceRepository crawlServiceRepository;
 
     @Autowired
+    private OperationVariableService operationVariableService;
+
+    @Autowired
     private LocationStatisticResponseMapper locationStatisticResponseMapper;
 
-    @GetMapping(value = "/vector/map/country", produces = "image/svg+xml")
+    @GetMapping(value = "/vector/map/country.svg", produces = "image/svg+xml")
     public String vectorMapCountry(
             @RequestParam(required = false, defaultValue = "dark", name = "style") String styleType,
             @RequestParam(required = false, defaultValue = "false", name = "stroke") boolean withStroke,
             Model model
-            ) {
+    ) {
         Map<String, String> federalStateColors = new HashMap<>();
         List<String> federalStates = List.of("la", "ua", "bl", "st", "ty", "ct", "sb", "vi", "vb");
 
@@ -81,6 +89,45 @@ public class EmbedController {
         model.addAttribute("disabledFill", styleType.equals("dark") ? "#1F2225" : "#BFC1C3");
 
         return "embed/vector/at";
+}
+
+    @GetMapping(value = "/vector/map/{federalState}.svg", produces = "image/svg+xml")
+    public String vectorMapFederalState(
+            @PathVariable String federalState,
+            @RequestParam(required = false, defaultValue = "dark", name = "style") String styleType,
+            @RequestParam(required = false, defaultValue = "false", name = "stroke") boolean withStroke,
+            Model model
+    ) {
+        federalState = operationVariableService.getFederalState(federalState);
+        System.out.println("federalState: " + federalState);
+        if(federalState == null || federalState.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Federal state not found");
+        }
+        Map<String, String> districtColors = new HashMap<>();
+
+        // get statistics
+        List<Operation> operations = operationService.getActiveOperationsByFederalState(federalState);
+        List<LocationStatisticResponse> statisticResponses = new ArrayList<>();
+        if (operations != null && !operations.isEmpty()) {
+            statisticResponses = locationStatisticResponseMapper.fromOperationDistrict(operations, federalState);
+        }
+
+        for (LocationStatisticResponse stat : statisticResponses) {
+            districtColors.put(stat.getNameId(), getMapTintColor(stat.getCountActive(), styleType.equals("dark")));
+        }
+
+        // generate style
+        String svgStyle = "#stroke { fill: " + (styleType.equals("dark") ? "#151718" : "#fff") + "}" +
+                "#map { fill: " + (styleType.equals("dark") ? "#1F2225" : "#BFC1C3" ) + "; } ";
+
+        for (Map.Entry<String, String> entry : districtColors.entrySet()) {
+            svgStyle += "#" + entry.getKey().toLowerCase() + " { fill: " + entry.getValue() + "; } ";
+        }
+
+        model.addAttribute("style", svgStyle);
+        model.addAttribute("showStroke", withStroke);
+
+        return "embed/vector/" + operationVariableService.getFederalStateId(federalState);
     }
 
     private String getMapTintColor(Long operationCount, Boolean isDarkMode) {
