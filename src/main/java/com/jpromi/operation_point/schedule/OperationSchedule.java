492efaf824d33ef7b31aad2571e753a00effb47b
ByAlexius <personal.git@my-server.at>
2025-11-25 17:38:10 +0100

    feat: add actual logging

--- src/main/java/com/jpromi/operation_point/schedule/OperationSchedule.java
+++ src/main/java/com/jpromi/operation_point/schedule/OperationSchedule.java
@@ -5,71 +5,100 @@ import com.jpromi.operation_point.entity.CrawlService;
 import com.jpromi.operation_point.repository.CrawlServiceRepository;
 import com.jpromi.operation_point.service.ApiOperationService;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OperationSchedule {
+    private final ApiOperationService apiOperationService;
+    private final CrawlServiceRepository crawlServiceRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private ApiOperationService apiOperationService;
-
-    @Autowired
-    private CrawlServiceRepository crawlServiceRepository;
+    public OperationSchedule(ApiOperationService apiOperationService, CrawlServiceRepository crawlServiceRepository) {
+        this.apiOperationService = apiOperationService;
+        this.crawlServiceRepository = crawlServiceRepository;
+    }
 
     @Scheduled(fixedRate = 120000) // 120 seconds
     @Transactional
     public void runUpdateOperations() {
-        if(isServiceActive("fs-ua")) updateUpperAustria(); else System.out.println("Upper Austria service is not active.");
-        if(isServiceActive("fs-la")) updateLowerAustria(); else System.out.println("Lower Austria service is not active.");
-        if(isServiceActive("fs-st")) updateStyria(); else System.out.println("Styria service is not active.");
-        if(isServiceActive("fs-ty")) updateTyrol(); else System.out.println("Tyrol service is not active.");
-        if(isServiceActive("fs-bl")) updateBurgenland(); else System.out.println("Burgenland service is not active.");
+        if(isServiceActive("fs-ua")) {
+            updateUpperAustria();
+        } else {
+            logger.warn("Upper Austria service is not active.");
+        }
+
+        if(isServiceActive("fs-la")) {
+            updateLowerAustria();
+        } else {
+            logger.warn("Lower Austria service is not active.");
+        }
+        
+        if(isServiceActive("fs-st")) {
+            updateStyria();
+        } else {
+            logger.warn("Styria service is not active.");
+        }
+        
+        if(isServiceActive("fs-ty")) {
+            updateTyrol();
+        } else {
+            logger.warn("Tyrol service is not active.");
+        }
+        
+        if(isServiceActive("fs-bl")) {
+            updateBurgenland();
+        } else {
+            logger.warn("Burgenland service is not active.");
+        }
     }
 
     private void updateTyrol() {
-        System.out.println("Updating Tyrol operations...");
+        logger.info("Updating Tyrol operations...");
         try {
             apiOperationService.getOperationListTyrol();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
     }
 
     private void updateUpperAustria() {
-        System.out.println("Updating Upper Austria operations...");
+        logger.info("Updating Upper Austria operations...");
         try {
             apiOperationService.getOperationListUpperAustria();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
     }
 
     private void updateStyria() {
-        System.out.println("Updating Styria operations...");
+        logger.info("Updating Styria operations...");
         try {
             apiOperationService.getOperationListStyria();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
     }
 
     private void updateBurgenland() {
-        System.out.println("Updating Burgenland operations...");
+        logger.info("Updating Burgenland operations...");
         try {
             apiOperationService.getOperationListBurgenland();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
     }
 
     private void updateLowerAustria() {
-        System.out.println("Updating Lower Austria operations...");
+        logger.info("Updating Lower Austria operations...");
         try {
             apiOperationService.getOperationListLowerAustria();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
     }
 
